e848dac15f9322c7289dfeec8c0102d399d84a1f
Mezz <mezzanine510@gmail.com>
2019-10-27 15:27:37 -0700

    Testing multiple elements within the template variable

--- src/app.js
+++ src/app.js
@@ -5,7 +5,17 @@ require('./app.css');
 console.log('This be changin');
 
 // JSX - JavaScript XML
-var template = <p>The world be changin'</p>
+var template = (
+    <div>
+        <h1>The world be changin'</h1>
+        <p>We be testin' also as well.</p>
+        <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+        </ul>
+    </div>
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
