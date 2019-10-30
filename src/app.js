d69e17980151b19c3e835823103beb6db17fb8ce
mezz <mezzanine510@gmail.com>
2019-10-29 23:32:38 -0700

    conditional rendering of elements test

--- src/app.js
+++ src/app.js
@@ -6,23 +6,25 @@ console.log('This be changin');
 
 const app = {
     title: 'Mai Taitle',
-    subtitle: 'Mai Subtaitle'
+    subtitle: 'Mai Subtaitle',
+    options: [ 'One', 'Two' ]
 }
 // JSX - JavaScript XML
 const template = (
     <div>
         <h1>{ app.title }</h1>
-        <p>{ app.subtitle }</p>
+        { app.subtitle && <p>{ app.subtitle }</p> }
+        { app.options.length > 0 ? <p>Here are your options</p> : <p>No options</p> }
         <ul>
-            <li>Item 1</li>
-            <li>Item 2</li>
+            <li>{ app.options[0] }</li>
+            <li>{ app.options[1] }</li>
         </ul>
     </div>
 );
 
-let user = {
+const user = {
     name: 'Nahuel',
-    age: '34',
+    age: 34,
     location: 'Earth'
 }
 
@@ -35,11 +37,11 @@ const getLocation = (location) => {
 const templateTwo = (
     <div>
         <h1>{ user.name ? user.name : 'Anonymous' }</h1>
-        <p>Age: { user.age }</p>
+        { user.age >= 18 && <p>Age: { user.age }</p> }
         { getLocation(user.location) }
     </div>
 );
 
-var appRoot = document.getElementById('app');
+const appRoot = document.getElementById('app');
 
-ReactDOM.render(templateTwo, appRoot);
+ReactDOM.render(template, appRoot);
