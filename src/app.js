b95e0ba739b739f078cac6bb2ab4c4f8dbe59620
Mezz <mezzanine510@gmail.com>
2019-11-05 22:32:58 -0800

    added some user interaction with buttons

--- src/app.js
+++ src/app.js
@@ -25,26 +25,32 @@ const template = (
     </div>
 );
 
-const user = {
-    name: 'Nahuel',
-    age: 34,
-    location: 'Earth'
+let count = 0;
+const addOne = () => {
+    console.log('Add one');
+    count++;
 }
 
-const getLocation = (location) => {
-    if (location) {
-        return <p>Location: { location }</p>
-    }
+const subtractOne = () => {
+    console.log('Subtract one');
+    count--;
+}
+
+const resetCount = () => {
+    console.log('Reset');
+    count = 0;
 }
 
 const templateTwo = (
     <div>
-        <h1>{ user.name ? user.name : 'Anonymous' }</h1>
-        { user.age >= 18 && <p>Age: { user.age }</p> }
-        { getLocation(user.location) }
+        <h1>Count: { count }</h1>
+        <button onClick={ addOne }>+1</button>
+        <button onClick={ subtractOne }>-1</button>
+        <button onClick={ resetCount }>reset</button>
     </div>
 );
 
+console.log(templateTwo);
 const appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
