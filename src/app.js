f96558312bde45d4f2a455629769fd4efb67c173
mezz <mezzanine510@gmail.com>
2019-11-16 16:03:10 -0800

    incorporated live reloads without page refresh

--- src/app.js
+++ src/app.js
@@ -1,34 +1,53 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
-require('./app.css');
+require('./app.css'); // for CSS loader
 
-console.log('This be changin');
-
+const appRoot = document.getElementById('app');
 const app = {
     title: 'Mai Taitle',
     subtitle: 'Mai Subtaitle',
-    options: [ 'Item One', 'Item Two' ]
+    options: []
 }
 
-// JSX - JavaScript XML
-const template = (
-    <div>
-        <h1>{ app.title }</h1>
+const onFormSubmit = (e) => {
+    e.preventDefault();
+    const option = e.target.elements.option.value;
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = '';
 
-        <p>{ app.subtitle }</p>
-        { app.subtitle && <p>{ app.subtitle }</p> }
-        { app.options.length > 0 ? <p>Here are your options</p> : <p>No options</p> }
-        <ul>
-            <li>{ app.options[0] }</li>
-            <li>{ app.options[1] }</li>
-        </ul>
+        renderApp();
+    }
+}
 
-    <form>
-        <input type="text" name="option"/>
-        <button>Add Option</button>
-    </form>
-    </div>
-);
+const removeAll = () => {
+    app.options = [];
+    renderApp();
+}
 
-const appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+const renderApp = () => {
+    const template = (
+        <div>
+            <h1>{ app.title }</h1>
+    
+            <p>{ app.subtitle }</p>
+            { app.subtitle && <p>{ app.subtitle }</p> }
+            { app.options.length > 0 ? <p>Here are your options</p> : <p>No options</p> }
+            <p>{ app.options.length }</p>
+            <button onClick={ removeAll }>Remove All</button>
+            <ol>
+                <li>{ app.options[0] }</li>
+                <li>{ app.options[1] }</li>
+            </ol>
+    
+        <form onSubmit={ onFormSubmit }>
+            <input type="text" name="option"/>
+            <button>Add Option</button>
+        </form>
+        </div>
+    );
+
+    ReactDOM.render(template, appRoot);
+}
+
+renderApp();
