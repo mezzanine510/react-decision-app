d5994b410bca6d6b8939d25a53c6bdf6f99e9385
Mezz <mezzanine510@gmail.com>
2019-11-26 00:13:02 -0800

    built on classes

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -1,7 +1,7 @@
 class Person {
     constructor(firstName, lastName, age) {
-        this.firstName = firstName || 'test first name';
-        this.lastName = lastName || 'test last name';
+        this.firstName = firstName || 'firstName';
+        this.lastName = lastName || 'lastName';
         this.age = age || 34;
     }
 
@@ -18,11 +18,45 @@ class Person {
     }
 }
 
-const me = new Person('Nahuel', 'Ruda');
+class Student extends Person {
+    constructor(firstName, lastName, age, major) {
+        super(firstName, lastName, age);
+        this.major = major;
+    }
+
+    hasMajor() {
+        return !!this.major;
+    }
+
+    getDescription() {
+        let description = super.getDescription();
+        if (this.hasMajor()) {
+            description += `Their major is ${ this.major }`;
+        }
+        return `${ this.firstName } is ${ this.age } years old and majors in ${ this.major }.`;
+    }
+}
+
+class Traveler extends Person {
+    constructor(firstName, lastName, age, homeLocation) {
+        super(firstName, lastName, age);
+        this.homeLocation = homeLocation;
+    }
+
+    getGreeting() {
+        let greeting = super.getGreeting();
+
+        if (this.homeLocation) {
+            greeting += ` I am visiting from ${ this.homeLocation }.`;
+        }
+        
+        return greeting;
+    }
+}
+
+const me = new Traveler('Nahuel', 'Ruda', 30, 'Rawrland');
 const otherPerson = new Person();
 
-const myName = me.getName();
 console.log(me.getName());
-console.log(myName);
-console.log(me.getGreeting());
 console.log(me.getDescription());
+console.log(me.getGreeting());
