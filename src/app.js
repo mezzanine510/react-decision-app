f64718bcc68487db50e8a0bc75d3186f5484c003
Mezz <mezzanine510@gmail.com>
2020-01-15 12:01:30 -0800

    added dafault prop values for Header title and conditional subtitle logic

--- src/app.js
+++ src/app.js
@@ -65,9 +65,13 @@ const Header = (props) => {
     return (
         <div>
             <h1>{ props.title }</h1>
-            <h2>{ props.subtitle }</h2>
+            { props.subtitle && <h2>{ props.subtitle }</h2> }
         </div>
     );
+}
+
+Header.defaultProps = {
+    title: 'Indecision'
 }
 
 const Action = (props) => {
