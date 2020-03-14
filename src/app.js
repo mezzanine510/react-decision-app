d05906306abf4322592f8bb2be48cf69981e9ae7
Mezz <mezzanine510@gmail.com>
2020-03-14 14:46:38 -0700

    added normalize.css

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import DecisionApp from './components/DecisionApp';
+import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 ReactDOM.render(<DecisionApp />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
                 use: 'babel-loader'
             },
             {
-                test: /\.(scss)$/,
+                test: /\.(s?css)$/,
                 use: [
                     'style-loader',
                     'css-loader',
